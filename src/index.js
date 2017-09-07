7466873641b670336aaff4ca95b17e0ceb608fa6
vinsol <vinsol@vinsols-MacBook-Pro.local>
2017-09-07 16:04:19 +0530

    slight changes to code to alternate players

--- src/index.js
+++ src/index.js
@@ -18,10 +18,13 @@ class Board extends React.Component {
     super();
     this.state ={
       squares: Array(9).fill(null),
+      xIsNext: true,
     };
   }
   handleClick(i) {
     const squares = this.state.squares.slice();
+    squares[i] = this.state.xIsNext?'X':'O';
+    this.setState({squares: squares, xIsNext: !this.state.xIsNext});
   }
   renderSquare(i) {
     return <Square 
